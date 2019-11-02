f28d4431dcb8a81c294df8e8aa173062942245f3
Adryan Hawkins <teamzaal@gmail.com>
2019-11-02 23:03:53 +0000

    Lesson 31

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,7 +1,8 @@
 class Instructor::LessonsController < ApplicationController
-before_action :authenticate_user!
-before_action :require_authorized_for_current_section
-
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_section, only: [:new, :create]
+	before_action :require_authorized_for_current_lesson, only: [:update]
+  
   def new
     @lesson = Lesson.new
   end
@@ -10,9 +11,18 @@ before_action :require_authorized_for_current_section
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
-
+	
+	def update
+		current_lesson.update_attributes(lesson_params)
+		render plain: 'Updated!'
+	end
+	
   private
-
+  
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+  
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
@@ -25,6 +35,6 @@ before_action :require_authorized_for_current_section
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
